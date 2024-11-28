ecc1a6cbd44d464024f014152d1ab8f93e2ee20f
Yuriy Nabasov <nabasoff@gmail.com>
2024-11-28 16:24:31 +0200

    Initial commit

---
+++ js/task-3.js
@@ -0,0 +1,102 @@
+//? Задача 3. Сортування за кількістю друзів
+// Напиши стрілочну функцію sortByDescendingFriendCount(users),
+// яка прийматиме один параметр users — масив об’єктів користувачів.
+// Функція має повертати масив усіх користувачів,
+// відсортованих за спаданням кількостій їх друзів(властивість friends).
+//! На що буде звертати увагу ментор при перевірці:
+// Оголошена змінна sortByDescendingFriendCount
+// Змінній sortByDescendingFriendCount присвоєна стрілочна функція з параметром (users)
+// Для перебирання параметра users використаний метод toSorted()
+// Виклик функції із зазначеним масивом users повертає новий масив користувачів,
+// відсортований за спаданням кількості їхніх друзів
+// Виклик функції з випадковими, але валідними аргументами повертає правильне значення
+
+`use strict`; // Код у суворому режимі
+
+//* Повна версія стрілочної функції:
+// const sortByDescendingFriendCount = (users) => {
+//   return users.toSorted((a, b) => b.friends.length - a.friends.length);
+// };
+
+//* Скорочена версія стрілочної функції:
+const sortByDescendingFriendCount = (users) =>
+  users.toSorted((a, b) => b.friends.length - a.friends.length);
+
+console.log(
+  sortByDescendingFriendCount([
+    {
+      name: "Moore Hensley",
+      friends: ["Sharron Pace"],
+      gender: "male",
+    },
+    {
+      name: "Sharlene Bush",
+      friends: ["Briana Decker", "Sharron Pace"],
+      gender: "female",
+    },
+    {
+      name: "Ross Vazquez",
+      friends: ["Marilyn Mcintosh", "Padilla Garrison", "Naomi Buckner"],
+      gender: "male",
+    },
+    {
+      name: "Elma Head",
+      friends: ["Goldie Gentry", "Aisha Tran"],
+      gender: "female",
+    },
+    {
+      name: "Carey Barr",
+      friends: ["Jordan Sampson", "Eddie Strong"],
+      gender: "male",
+    },
+    {
+      name: "Blackburn Dotson",
+      friends: ["Jacklyn Lucas", "Linda Chapman"],
+      gender: "male",
+    },
+    {
+      name: "Sheree Anthony",
+      friends: ["Goldie Gentry", "Briana Decker"],
+      gender: "female",
+    },
+  ])
+);
+
+//*Результат повинен бути:
+// [
+//   {
+//     name: "Ross Vazquez",
+//     friends: ["Marilyn Mcintosh", "Padilla Garrison", "Naomi Buckner"],
+//     gender: "male",
+//   },
+//   {
+//     name: "Sharlene Bush",
+//     friends: ["Briana Decker", "Sharron Pace"],
+//     gender: "female",
+//   },
+//   {
+//     name: "Elma Head",
+//     friends: ["Goldie Gentry", "Aisha Tran"],
+//     gender: "female",
+//   },
+//   {
+//     name: "Carey Barr",
+//     friends: ["Jordan Sampson", "Eddie Strong"],
+//     gender: "male",
+//   },
+//   {
+//     name: "Blackburn Dotson",
+//     friends: ["Jacklyn Lucas", "Linda Chapman"],
+//     gender: "male",
+//   },
+//   {
+//     name: "Sheree Anthony",
+//     friends: ["Goldie Gentry", "Briana Decker"],
+//     gender: "female",
+//   },
+//   {
+//     name: "Moore Hensley",
+//     friends: ["Sharron Pace"],
+//     gender: "male",
+//   },
+// ];
